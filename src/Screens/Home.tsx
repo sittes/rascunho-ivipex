427e9b39203ed7ecd03fdb5e4848a323ae6315ca
sittes <paulo.fagnerdeoliveira@gmail.com>
2024-11-01 22:42:35 -0300

    atualizando projeto

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -15,7 +15,7 @@ export default Home
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
